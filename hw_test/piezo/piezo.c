3f27f4d1d230673343f5f48e3f93af99df467ded
Matej Pončák <poncak.matej1@gmail.com>
2022-04-29 22:28:41 +0200

    added easy config for piezo example

--- hw_test/piezo/piezo.c
+++ hw_test/piezo/piezo.c
@@ -4,20 +4,26 @@
 
 #define PIEZO 10
 
+#define START_FREQ 800u
+#define STOP_FREQ 1200u
+#define STEP_FREQ 2u
+
+#define ONE_TONE_TIME 8000ul
+
 int main() {
     gpio_init(PIEZO);
 
     gpio_set_dir(PIEZO, GPIO_OUT);
 
-    uint64_t frequency = 800;
-    int16_t step = 2;
+    uint64_t frequency = START_FREQ;
+    int16_t step = STEP_FREQ;
     bool state = false;
 
     while (1) {
         uint64_t delay_time = 1000000ul / frequency;
 
         uint64_t start = time_us_64();
-        while((start + 8000ul) > time_us_64())
+        while((start + ONE_TONE_TIME) > time_us_64())
         {
             gpio_put(PIEZO, state);
             state = !state;
@@ -26,7 +32,7 @@ int main() {
 
         frequency += step;
 
-        if(frequency >= 1200) step = -step;
-        if(frequency <= 800) step = -step;
+        if(frequency >= STOP_FREQ) step = -step;
+        if(frequency <= START_FREQ) step = -step;
     }
 }
