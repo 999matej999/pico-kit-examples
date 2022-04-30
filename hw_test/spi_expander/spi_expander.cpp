294323b6069398ad70d9dc9603bae5d742912dc3
Matej Pončák <poncak.matej1@gmail.com>
2022-04-30 23:12:05 +0200

    little changes

--- hw_test/spi_expander/spi_expander.cpp
+++ hw_test/spi_expander/spi_expander.cpp
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
 #include "SPI.h"
 #include "MCP23S018.h"
 
 int main()
 {
+    stdio_init_all();
+    printf("SPI expander example\n");
+
     SPI spi;
 
     MCP23S018 expander(spi);
 
-    stdio_init_all();
-
-    printf("SPI expander example\n");
+    expander.setDirection(0xF0);
 
     while(true)
     {
-        uint8_t buffer = 0;
         uint8_t value = expander.read();
-        expander.setDirection(0xF0);
-        expander.write((value & 0xF0) >> 4);
 
-        for (size_t i = 0; i < 8; ++i)
+        for (int8_t i = 7; i >= 0; --i)
         {
-            printf("%c", (buffer & (1 << i)) ? '1' : '0');
+            printf("%c", (value & (1 << i)) ? '1' : '0');
         }
         printf("\n");
+        
+        expander.write((value & 0xF0) >> 4);
 
         sleep_ms(10);
     }
